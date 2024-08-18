ea9e77b98259d31d345a77bfae33f607c92bbcfb
Cemlyn <cemlynwaters@gmail.com>
2024-08-19 00:30:55 +0100

    feat: downgrade requested opengl to support mac

--- tictactoe_ai/renderer/renderer.cc
+++ tictactoe_ai/renderer/renderer.cc
@@ -29,7 +29,7 @@ void init() {
   }
   // else...
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 };
--- tictactoe_ai/renderer/shaders/shader.vert
+++ tictactoe_ai/renderer/shaders/shader.vert
@@ -1,6 +1,6 @@
-#version 430 core
+#version 410 core
 layout (location = 0) in vec3 aPos;
-layout (location = 1) uniform vec2 offset;
+uniform vec2 offset;
 void main()
 {
    gl_Position = vec4(aPos.x + offset.x, aPos.y + offset.y, aPos.z, 1.0);
